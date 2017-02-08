6882eb3dc7ab16c1803375f5de0f65d2e742e15d
alex85vol <alex85vol@gmail.com>
2017-02-08 18:36:39 +0200

    added adapter and golovach

--- src/com/learning/patterns/adapter/AdapterPatternDemo.java
+++ src/com/learning/patterns/adapter/AdapterPatternDemo.java
@@ -12,6 +12,6 @@ public class AdapterPatternDemo {
         audioPlayer.play("mp3", "beyond the horizon.mp3");
         audioPlayer.play("mp4", "alone.mp4");
         audioPlayer.play("vlc", "far far away.vlc");
-        audioPlayer.play("avi", "mind me.avi");
+        audioPlayer.play("avi", "mind me.avig");
     }
 }
